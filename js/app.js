70f2adeb7633537cee6e6afc5d93f064d4a1bf07
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2020-10-21 12:44:07 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-const tasks = [];
 const todoInput = document.querySelector(".todo-input");
 const btnAdd = document.querySelector("#btnAdd");
 const newTaskText = document.querySelector(".new-task-text");
@@ -13,23 +11,59 @@ btnAdd.addEventListener("click", () => {
 
 todoInput.addEventListener("keypress", e => {
 	if(e.key === "Enter") {
-		const task = e.target.value
-		tasks.unshift(new Task(task,index++))
+		const task = e.target.value;
+		const taskElement =  makeTaskElement(task);
+		const todoList = document.querySelector(".todo-list");
+		const taskCheckComplete = taskElement.querySelector("input[type=checkbox]");
+		const removeBtn = taskElement.querySelector("a");
+		bindTaskEvent(taskCheckComplete, "click", completeTask)
+		bindTaskEvent(removeBtn, "click", deleteTask)
+		todoList.prepend(taskElement)
 		e.target.value = "";
 		e.target.classList.toggle("hidden")
 		newTaskText.classList.toggle("hidden")
+
+
 	}
 })
 
-class Task {
-	constructor(task , i) {
-		this.task = task;
-		this.status = status = "incomplete";
-		this.i = i;
-	}
+const makeTaskElement = function(task) {
+	/*const template = `<li>
+		<input type="checkbox">
+		<label for="">${task}</label>
+		<a href="#"></a>
+		</li>`;
+	document.querySelector('.todo-list').innerHTML += template;
+	//document.querySelector('.todo-list').textContent += template;*/
+	const li = document.createElement('li');
+	const checkbox = document.createElement('input')
+	const label = document.createElement('label')
+	const a = document.createElement('a');
 
-	completeTask () {
-		this.status = "complete"
-	}
+	checkbox.type = "checkbox";
+	label.textContent = task;
+	a.href = "#";
+
+	li.appendChild(checkbox);
+	li.appendChild(label);
+	li.appendChild(a);
+
+	return li
 }
 
+const bindTaskEvent = function(el, event, fn) {
+	el.addEventListener(event, fn)
+}
+
+const completeTask = function(e) {
+	const parent = e.target.parentNode;
+	const labelTask = parent.querySelector("label");
+	labelTask.classList.toggle("task-complete")
+}
+
+const deleteTask = function(e) {
+	const parent = e.target.parentNode;
+	const parentList = parent.parentNode;
+
+	parentList.removeChild(parent)
+}
