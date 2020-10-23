5b244114f0ccd482a44ba221dae40026897f704d
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2020-10-23 13:06:13 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -2,14 +2,43 @@ const todoInput = document.querySelector(".todo-input");
 const btnAdd = document.querySelector("#btnAdd");
 const newTaskText = document.querySelector(".new-task-text");
 const btnLoggin = document.querySelector("#btnLoggin");
+const btnLogout = document.querySelector("#logout");
+let tasks = [];
 
+loadTask()
 
+/*const loggin = function (user){
+	localStorage.setItem("user", user )
+	hiddenLoggin()
+}
+
+const logout = function(){
+	localStorage.removeItem("user")
+	document.querySelector(".container-todo").classList.add("hidden")
+	document.querySelector(".loggin").classList.remove("hidden")
+}
+
+const isLoggin = function() {
+	return localStorage.getItem("user")
+}
+
+btnLogout.addEventListener('click',logout)
+
+if(isLoggin()) {
+	document.querySelector(".container-todo").classList.remove("hidden")
+	document.querySelector(".loggin").classList.add("hidden")
+}
+
+const hiddenLoggin = function() {
+	document.querySelector(".container-todo").classList.remove("hidden")
+	document.querySelector(".loggin").classList.add("hidden")
+}
+*/
 
 btnLoggin.addEventListener("click", () => {
 	const user = document.querySelector("#user").value;
 	const password = document.querySelector("#password").value;
-
-	document.cookie = `saludo=${user}; max-age=0;`;
+	loggin(user)
 })
 
 btnAdd.addEventListener("click", () => {
@@ -28,6 +57,8 @@ todoInput.addEventListener("keypress", e => {
 		bindTaskEvent(taskCheckComplete, "click", completeTask)
 		bindTaskEvent(removeBtn, "click", deleteTask)
 		todoList.prepend(taskElement)
+		const id = new Date().getTime();
+		storageTask(id, task)
 		e.target.value = "";
 		e.target.classList.toggle("hidden")
 		newTaskText.classList.toggle("hidden")
@@ -36,7 +67,20 @@ todoInput.addEventListener("keypress", e => {
 	}
 })
 
-const makeTaskElement = function(task) {
+function loadTask() {
+	tasks = JSON.parse(localStorage.getItem("tasks")) || [] 
+	for(let task of tasks) {
+		const taskElement =  makeTaskElement(task.task);
+		const todoList = document.querySelector(".todo-list");
+		const taskCheckComplete = taskElement.querySelector("input[type=checkbox]");
+		const removeBtn = taskElement.querySelector("a");
+		bindTaskEvent(taskCheckComplete, "click", completeTask)
+		bindTaskEvent(removeBtn, "click", deleteTask)
+		todoList.prepend(taskElement)
+	}
+}
+
+function makeTaskElement(task) {
 	/*const template = `<li>
 		<input type="checkbox">
 		<label for="">${task}</label>
@@ -60,18 +104,18 @@ const makeTaskElement = function(task) {
 	return li
 }
 
-const bindTaskEvent = function(el, event, fn) {
+function bindTaskEvent(el, event, fn) {
 	el.addEventListener(event, fn)
 }
 
-const completeTask = function(e) {
+ function completeTask(e) {
 	const parent = e.target.parentNode;
 	parent.classList.toggle("task-complete")
 	const taskCompleteNumber = document.querySelector(".task-complete-number span")
 	taskCompleteNumber.textContent = countTaskCompletes();	
 }
 
-const deleteTask = function(e) {
+function deleteTask(e) {
 	const parent = e.target.parentNode;
 	const parentList = parent.parentNode;
 
@@ -82,4 +126,18 @@ const deleteTask = function(e) {
 
 const countTaskCompletes = function() {
 	return document.querySelectorAll("li.task-complete").length
+}
+
+const storageTask = function (id, task) {
+	tasks.push({
+		id: id,
+		task: task
+	})
+	saveLocalStorage(tasks)
+}
+
+
+
+const saveLocalStorage = function(tasks) {
+	localStorage.setItem("tasks", JSON.stringify(tasks))
 }
